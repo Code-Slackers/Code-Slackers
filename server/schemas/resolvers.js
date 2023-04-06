3fabf0bec4f9133f73e3f6bf376deeeab63c2bb9
Liquid Monks <info@liquidmonks.com>
2023-04-06 18:31:48 -0500

    Includes context.user token to header and google map libraries.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -13,7 +13,7 @@ const resolvers = {
     },
     // By adding context to our query, we can retrieve the logged in user without specifically searching for them
     me: async (parent, args, context) => {
-      if (context.user) {
+      if (context.loggedIn) {
         return Profile.findOne({ _id: context.user._id });
       }
       throw new AuthenticationError("You need to be logged in!");
@@ -82,109 +82,109 @@ const resolvers = {
     },
 
     addTrip: async (parent, { locationId, profileId, dateOfTrip }, context) => {
-      // if (context.user) {
-      const createTrip = await Trips.create({ profileId, dateOfTrip });
-      await Profile.findOneAndUpdate(profileId, { $push: { trips: createTrip._id } }, { new: true });
-      await Location.findOneAndUpdate(locationId, { $push: { trips: createTrip._id } }, { new: true });
-      return createTrip;
+      if (context.loggedIn) {
+        const createTrip = await Trips.create({ profileId, dateOfTrip });
+        await Profile.findOneAndUpdate(profileId, { $push: { trips: createTrip._id } }, { new: true });
+        await Location.findOneAndUpdate(locationId, { $push: { trips: createTrip._id } }, { new: true });
+        return createTrip;
+      }
+      throw new AuthenticationError("You need to be logged in!");
     },
-    // throw new AuthenticationError("You need to be logged in!");
-    // },
 
     addLocation: async (parent, { profileId, city, state }, context) => {
-      // if (context.user) {
-      const createLocation = await Location.create({ profileId, city, state });
-      return createLocation;
+      if (context.loggedIn) {
+        const createLocation = await Location.create({ profileId, city, state });
+        return createLocation;
+      }
+      throw new AuthenticationError("You need to be logged in!");
     },
-    // throw new AuthenticationError("You need to be logged in!");
-    // },
 
     updateTrip: async (parent, args, context) => {
-      // if (context.user) {
-      const updatedTrip = await Trips.findOneAndUpdate({ _id: args.tripId }, args, { new: true });
-      return await updatedTrip.populate(["food", "lodging", "thingsToDo", "transportation"]);
-      // }
-      // throw new AuthenticationError("You need to be logged in!");
+      if (context.loggedIn) {
+        const updatedTrip = await Trips.findOneAndUpdate({ _id: args.tripId }, args, { new: true });
+        return await updatedTrip.populate(["food", "lodging", "thingsToDo", "transportation"]);
+      }
+      throw new AuthenticationError("You need to be logged in!");
     },
 
     updateLocation: async (parent, args, context) => {
-      // if (context.user) {
-      const updatedLocation = await Location.findOneAndUpdate({ _id: args.locationId }, args, { new: true });
-      return await updatedLocation.populate(["food", "lodging", "thingsToDo", "transportation", "visitors"]);
-      // }
-      // throw new AuthenticationError("You need to be logged in!");
+      if (context.loggedIn) {
+        const updatedLocation = await Location.findOneAndUpdate({ _id: args.locationId }, args, { new: true });
+        return await updatedLocation.populate(["food", "lodging", "thingsToDo", "transportation", "visitors"]);
+      }
+      throw new AuthenticationError("You need to be logged in!");
     },
 
     addFood: async (parent, { locationId, profileId, city, state, address, phone, category, cost, images, reviews, starRating }, context) => {
-      // if (context.user) {
-      const addFood = await Food.create({ profileId, city, state, address, phone, category, cost, images, reviews, starRating });
-      const updateLocation = await Location.findOneAndUpdate(locationId, { $push: { food: addFood._id } }, { new: true });
-      return addFood;
-      // }
-      // throw new AuthenticationError("You need to be logged in!");
+      if (context.loggedIn) {
+        const addFood = await Food.create({ profileId, city, state, address, phone, category, cost, images, reviews, starRating });
+        const updateLocation = await Location.findOneAndUpdate(locationId, { $push: { food: addFood._id } }, { new: true });
+        return addFood;
+      }
+      throw new AuthenticationError("You need to be logged in!");
     },
 
     updateFood: async (parent, args, context) => {
-      // if (context.user) {
-      const updatedFood = await Food.findOneAndUpdate({ _id: args.foodId }, args, { new: true });
-      return await updatedFood;
-      // }
-      // throw new AuthenticationError("You need to be logged in!");
+      if (context.loggedIn) {
+        const updatedFood = await Food.findOneAndUpdate({ _id: args.foodId }, args, { new: true });
+        return updatedFood;
+      }
+      throw new AuthenticationError("You need to be logged in!");
     },
 
     addLodging: async (parent, { locationId, profileId, city, state, address, phone, category, cost, amenities, images, reviews, starRating }, context) => {
-      // if (context.user) {
-      const addLodging = await Lodging.create({ profileId, city, state, address, phone, category, cost, amenities, images, reviews, starRating });
-      await Location.findOneAndUpdate(locationId, { $push: { lodging: addLodging._id } }, { new: true });
-      return addLodging;
-      // }
-      // throw new AuthenticationError("You need to be logged in!");
+      if (context.loggedIn) {
+        const addLodging = await Lodging.create({ profileId, city, state, address, phone, category, cost, amenities, images, reviews, starRating });
+        await Location.findOneAndUpdate(locationId, { $push: { lodging: addLodging._id } }, { new: true });
+        return addLodging;
+      }
+      throw new AuthenticationError("You need to be logged in!");
     },
 
     updateLodging: async (parent, args, context) => {
-      // if (context.user) {
-      const updatedLodging = await Lodging.findOneAndUpdate({ _id: args.lodgingId }, args, { new: true });
-      return await updatedLodging;
-      // }
-      // throw new AuthenticationError("You need to be logged in!");
+      if (context.loggedIn) {
+        const updatedLodging = await Lodging.findOneAndUpdate({ _id: args.lodgingId }, args, { new: true });
+        return updatedLodging;
+      }
+      throw new AuthenticationError("You need to be logged in!");
     },
 
     addTransportation: async (parent, { locationId, profileId, city, state, address, phone, category, amenities }, context) => {
-      // if (context.user) {
-      const addTransportation = await Transportation.create({ profileId, city, state, address, phone, category, amenities });
-      await Location.findOneAndUpdate(locationId, { $push: { transportation: addTransportation._id } }, { new: true });
-      return addTransportation;
-      // }
-      // throw new AuthenticationError("You need to be logged in!");
+      if (context.loggedIn) {
+        const addTransportation = await Transportation.create({ profileId, city, state, address, phone, category, amenities });
+        await Location.findOneAndUpdate(locationId, { $push: { transportation: addTransportation._id } }, { new: true });
+        return addTransportation;
+      }
+      throw new AuthenticationError("You need to be logged in!");
     },
     updateTransportation: async (parent, args, context) => {
-      // if (context.user) {
-      const updatedTransportation = await Transportation.findOneAndUpdate({ _id: args.transportationId }, args, { new: true });
-      return await updatedTransportation;
-      // }
-      // throw new AuthenticationError("You need to be logged in!");
+      if (context.loggedIn) {
+        const updatedTransportation = await Transportation.findOneAndUpdate({ _id: args.transportationId }, args, { new: true });
+        return updatedTransportation;
+      }
+      throw new AuthenticationError("You need to be logged in!");
     },
 
     addThingsToDo: async (parent, { locationId, profileId, city, state, address, phone, category, cost, images, reviews, starRating }, context) => {
-      // if (context.user) {
-      const addThingsToDo = await ThingsToDo.create({ profileId, city, state, address, phone, category, cost, images, reviews, starRating });
-      await Location.findOneAndUpdate(locationId, { $push: { thingsToDo: addThingsToDo._id } }, { new: true });
-      return addThingsToDo;
-      // }
-      // throw new AuthenticationError("You need to be logged in!");
+      if (context.loggedIn) {
+        const addThingsToDo = await ThingsToDo.create({ profileId, city, state, address, phone, category, cost, images, reviews, starRating });
+        await Location.findOneAndUpdate(locationId, { $push: { thingsToDo: addThingsToDo._id } }, { new: true });
+        return addThingsToDo;
+      }
+      throw new AuthenticationError("You need to be logged in!");
     },
 
     updateThingsToDo: async (parent, args, context) => {
-      // if (context.user) {
-      const updatedThingsToDo = await ThingsToDo.findOneAndUpdate({ _id: args.thingsToDoId }, args, { new: true });
-      return await updatedThingsToDo;
-      // }
-      // throw new AuthenticationError("You need to be logged in!");
+      if (context.loggedIn) {
+        const updatedThingsToDo = await ThingsToDo.findOneAndUpdate({ _id: args.thingsToDoId }, args, { new: true });
+        return updatedThingsToDo;
+      }
+      throw new AuthenticationError("You need to be logged in!");
     },
 
     // Set up mutation so a logged in user can only remove their profile and no one else's
     removeProfile: async (parent, args, context) => {
-      if (context.user) {
+      if (context.loggedIn) {
         return Profile.findOneAndDelete({ _id: context.user._id });
       }
       throw new AuthenticationError("You need to be logged in!");
--- server/server.js
+++ server/server.js
@@ -11,7 +11,13 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware,
+  context: ({ req }) => {
+    const user = authMiddleware(req)
+    return {
+      user,
+      loggedIn: user.name ? true : false
+    }
+  },
 });
 
 app.use(express.urlencoded({ extended: false }));
@@ -30,14 +36,14 @@ app.get('/', (req, res) => {
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
-  
+
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     })
   })
-  };
-  
+};
+
 // Call the async function to start the server
-  startApolloServer(typeDefs, resolvers);
+startApolloServer(typeDefs, resolvers);
