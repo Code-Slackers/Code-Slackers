acd19e6ea8f3096d0d8d640a7f526ed1651abff0
chriscuchetto <chriscuchetto@gmail.com>
2023-04-06 20:38:44 -0500

    styling done to header and footer

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -9,32 +9,32 @@ const Header = () => {
     Auth.logout();
   };
   return (
-    <header className="bg-info text-dark mb-4 py-3 display-flex align-center">
-      <div className="container flex-column justify-space-between-lg justify-center align-center text-center">
+    <header className="bg-primary text-dark mb-4 py-3 flex items-center">
+      <div className="flex-col lg:flex-row lg:justify-between justify-center lg:items-center mx-auto text-center">
         <Link className="text-dark" to="/">
-          <h1 className="m-0" style={{ fontSize: '3rem' }}>
-            Tech Friends
+          <h1 className="m-0 text-secondary text-5xl font-bold pb-2">
+            tripr
           </h1>
         </Link>
-        <p className="m-0" style={{ fontSize: '1.75rem', fontWeight: '700' }}>
-          Meet your new programming pals.
+        <p className="mb-2 text-xl font-bold">
+          Find Your Next Adventure
         </p>
         <div>
           {Auth.loggedIn() ? (
             <>
-              <Link className="btn btn-lg btn-primary m-2" to="/me">
+              {/* <Link className="px-4 py-2 text-lg bg-secondary text-white rounded-md m-2" to="/me">
                 View My Profile
-              </Link>
-              <button className="btn btn-lg btn-light m-2" onClick={logout}>
+              </Link> */}
+              <button className="px-4 py-1 text-lg bg-secondary rounded-md m-2" onClick={logout}>
                 Logout
               </button>
             </>
           ) : (
             <>
-              <Link className="btn btn-lg btn-primary m-2" to="/login">
+              {/* <Link className="px-4 py-1 text-lg bg-secondary text-white rounded-md m-2" to="/login">
                 Login
-              </Link>
-              <Link className="btn btn-lg btn-light m-2" to="/signup">
+              </Link> */}
+              <Link className="px-4 py-1 text-lg bg-secondary rounded-md m-2" to="/signup">
                 Signup
               </Link>
             </>
