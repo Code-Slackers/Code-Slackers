e3f1302332f8698a9ff64e53b3f64ddc6148294c
Liquid Monks <info@liquidmonks.com>
2023-04-13 13:13:51 -0500

    Inludes additional design styling to component files.

--- client/src/components/LodgingByState/index.js
+++ client/src/components/LodgingByState/index.js
@@ -4,30 +4,28 @@ const LodgingByState = ({ lodgings }) => {
   if (!lodgings.length) {
     return (
       <div className="mt-5 text-center">
-        <h3>No Lodging Yet</h3>
+        <h3 className="text-2xl font-bold">No Lodging Yet</h3>
       </div>
     );
   }
 
   return (
-    <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
       {lodgings &&
         lodgings.map((lodging) => (
-          <div key={lodging._id} id={lodging._id} className="col">
-            <div className="card h-100">
-              <img src={lodging.images[0]} className="card-img-top" alt="lodging" />
-              <div className="card-body">
-                <h5 className="card-title">{lodging.category}</h5>
-                <p className="card-text">
-                  {lodging.address} {lodging.state}
-                </p>
-                <p className="card-text">
-                  {lodging.cost} {lodging.starRating}
-                </p>
-              </div>
-              <div className="card-footer">
-                <button className="btn btn-primary w-100">ADD LODGING TO TRIP</button>
-              </div>
+          <div key={lodging._id} id={lodging._id} className="overflow-hidden bg-white rounded-lg shadow-lg">
+            <img src={lodging.images[0]} className="object-cover w-full h-48" alt="lodging" />
+            <div className="p-4">
+              <h5 className="mb-2 text-lg font-bold">{lodging.category}</h5>
+              <p className="mb-2 text-gray-600">
+                {lodging.address} {lodging.state}
+              </p>
+              <p className="mb-4 text-gray-600">
+                {lodging.cost} {lodging.starRating}
+              </p>
+              <button className="block w-full px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-600">
+                ADD LODGING TO TRIP
+              </button>
             </div>
           </div>
         ))}
--- client/src/components/ThingsToDoByState/index.js
+++ client/src/components/ThingsToDoByState/index.js
@@ -2,25 +2,27 @@ import React from "react";
 
 const ThingsToDoByState = ({ thingsToDo }) => {
   if (!thingsToDo.length) {
-    return <h3>No Things To Do Yet</h3>;
+    return (
+      <div className="mt-5 text-center">
+        <h3>No Things To Do Yet</h3>
+      </div>
+    );
   }
 
   return (
-    <div className="flex flex-wrap justify-center">
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
       {thingsToDo &&
         thingsToDo.map((thingToDo) => (
-          <div key={thingToDo._id} id={thingToDo._id} className="p-4 mx-4 my-2 bg-white rounded-lg shadow-lg w-96">
-            <div className="mb-3 card">
-              <h4 className="text-2xl font-bold">{thingToDo.category}</h4>
+          <div key={thingToDo._id} id={thingToDo._id} className="bg-white rounded-lg shadow-lg">
+            <div className="h-56">
+              {!thingToDo.images.length ? null : <img className="object-cover w-full h-full rounded-t-lg" src={thingToDo.images[0]} alt="thingToDo" />}
             </div>
-            <div className="mb-2 text-lg">
-              {thingToDo.address} {thingToDo.state}
+            <div className="p-6">
+              <h4 className="mb-2 text-xl font-bold">{thingToDo.category}</h4>
+              <p className="mb-2 text-lg text-gray-700">{thingToDo.address} {thingToDo.state}</p>
+              <p className="mb-4 text-lg text-gray-700">{thingToDo.cost} {thingToDo.starRating}</p>
+              <button className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-lg hover:bg-blue-700">ADD THING TO DO TO TRIP</button>
             </div>
-            <div className="mb-4 text-lg">
-              {thingToDo.cost} {thingToDo.starRating}
-            </div>
-            {!thingToDo.images.length ? null : <img className="w-full rounded-md" src={thingToDo.images[0]} alt="thingToDo" />}
-            <button className="px-4 py-2 mt-4 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">ADD THING TO DO TO TRIP</button>
           </div>
         ))}
     </div>
