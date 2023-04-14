3e6f1aebd103154d0f94cd603ba16c4dc447789d
Liquid Monks <info@liquidmonks.com>
2023-04-14 07:08:18 -0500

    Includes additional stylling to pages and component forms.

--- client/src/pages/ViewSelections.js
+++ client/src/pages/ViewSelections.js
@@ -21,11 +21,14 @@ const ViewSelection = () => {
   };
 
   return (
-    <div id={params.locationId} className="flex flex-col">
-      <button onClick={ViewFood}>VIEW FOOD</button>
-      <button onClick={ViewTransportation}>VIEW TRANSPORTATION</button>
-      <button onClick={ViewThingsToDo}>VIEW THINGS TO DO</button>
-      <button onClick={ViewLodging}>VIEW LODGING</button>
+    <div id={params.locationId} className="flex flex-col items-center justify-center h-screen">
+      <h1 className="mb-8 text-3xl font-bold">View Selection</h1>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4">
+        <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50" onClick={ViewFood}>View Food</button>
+        <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50" onClick={ViewTransportation}>View Transportation</button>
+        <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50" onClick={ViewThingsToDo}>View Things to Do</button>
+        <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50" onClick={ViewLodging}>View Lodging</button>
+      </div>
     </div>
   );
 };
--- client/src/pages/ViewTransportation.js
+++ client/src/pages/ViewTransportation.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { QUERY_TRANSPORTATIONBYSTATE } from "../utils/queries";
@@ -11,7 +10,11 @@ const ViewTransportation = () => {
     variables: { locationId: locationId },
   });
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <div>Loading...</div>
+      </div>
+    );
   }
   const transportations = data?.location.transportation || [];
 
@@ -21,20 +24,20 @@ const ViewTransportation = () => {
 
   if (!transportations.length) {
     return (
-      <>
-        <h3 className="text-center">No Transportation Yet</h3>
-        <button className="px-4 py-2 mt-4 text-white bg-blue-500 rounded hover:bg-blue-600" onClick={addTransportationHandler}>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h3 className="mb-4 text-3xl font-bold text-center">No Transportation Yet</h3>
+        <button className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600" onClick={addTransportationHandler}>
           ADD TRANSPORTATION
         </button>
-      </>
+      </div>
     );
   }
 
   return (
-    <div id={locationId} className="flex flex-col">
-      <h1 className="mb-4 text-3xl font-bold">Transportation in {transportations[0].state}</h1>
+    <div id={locationId} className="flex flex-col items-center">
+      <h1 className="mb-8 text-3xl font-bold">Transportation in {transportations[0].state}</h1>
       <div className="flex flex-col items-center">
-        <button className="px-4 py-2 mt-4 text-white bg-blue-500 rounded hover:bg-blue-600" onClick={addTransportationHandler}>
+        <button className="px-4 py-2 mb-8 text-white bg-blue-500 rounded hover:bg-blue-600" onClick={addTransportationHandler}>
           ADD TRANSPORTATION
         </button>
         <div>{loading ? <div>Loading...</div> : <TransportationByState transportations={transportations} />}</div>
