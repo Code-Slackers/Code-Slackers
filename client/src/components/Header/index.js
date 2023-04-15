c9d1f33b38cac84036ffc9ce6654e31762321596
Liquid Monks <info@liquidmonks.com>
2023-04-15 10:29:32 -0500

    Header/index.js includes home menu that renders on desktop and mobile hamburger menu.

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,7 +1,7 @@
+import { ArrowLeftOnRectangleIcon, Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Auth from '../../utils/auth';
-import { ArrowLeftOnRectangleIcon, Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 
 const Header = () => {
   const [open, setOpen] = useState(false)
@@ -43,10 +43,15 @@ const Header = () => {
               {/* <Link className="px-4 py-2 m-2 text-lg text-white rounded-md bg-secondary" to="/me">
                 View My Profile
               </Link> */}
-              <button className="gap-1 btn btn-secondary btn-sm" onClick={logout}>
+              <button className="h-10 gap-1 btn btn-secondary btn-sm" onClick={logout}>
                 <ArrowLeftOnRectangleIcon className='w-6 h-6' />
                 Logout
               </button>
+              <Link to={'/'} className="flex items-center justify-center w-10 h-10 bg-white rounded-md hover:bg-slate-200">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 stroke-black">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+              </svg>
+            </Link>
             </div>
           ) : (
             <div className='flex items-center justify-center gap-3 mt-3'>
